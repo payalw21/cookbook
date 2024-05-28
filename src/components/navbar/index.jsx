d806cdb5c7d42d0a3507e8907293fb110304ad0b
Payal Wankhede <payalwankhede855@gmail.com>
2024-05-28 23:52:48 +0530

    Add navbar component

---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,42 @@
+import { NavLink } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav className="flex justify-between items-center py-8 container mx-auto flex-col lg:flex-row gap-5 lg:gap-0">
+      <h2 className="text-2xl font-semibold">
+        <NavLink
+          to={"/"}
+          className="text-black hover:text-gray-700 duration-300"
+        >
+          Cookbook
+        </NavLink>
+      </h2>
+      <form>
+        <input
+          type="text"
+          name="search"
+          placeholder="Search Here..."
+          className="bg-white/75 p-3 px-8 rounded-full outline-none lg:w-96 shadow-lg shadow-red-100 focus:shadow-red-200"
+        />
+      </form>
+      <ul className="flex gap-5">
+        <li>
+          <NavLink
+            to={"/"}
+            className="text-black hover:text-gray-700 duration-300"
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to={"/favorites"}
+            className="text-black hover:text-gray-700 duration-300"
+          >
+            Fovrites
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
